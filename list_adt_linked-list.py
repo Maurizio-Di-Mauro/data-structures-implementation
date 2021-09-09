b60dcdd0835c1646e3030106747824d8e83f5643
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-09-09 16:56:56 +0300

    remove an element by value also add Node and Linled List string form

--- list_adt_linked-list.py
+++ list_adt_linked-list.py
@@ -11,11 +11,17 @@ class Node:
         self.value = value
         self.next = next
 
+    def __str__(self):
+        return f'{{value: {self.value}; next: {self.next}}}'
+
 
 class LinkedList:
     """This is a Link List itself. It only contains a head node"""
     def __init__(self, head: Node):
         self.head = head
+
+    def __str__(self):
+        return f'[{self.head}]'
         
     # We have to do at least four basic operations with Linked List:
     #
@@ -54,23 +60,44 @@ class LinkedList:
         newHead = Node(value, self.head)
         self.head = newHead
 
+    # Remove an element by value
+    def remove(self, value: int):
+        # If there are multiple Nodes with the same value,
+        # remove the first occurence
+        current: Node = self.head
+        if current.value == value:
+            self.head = current.next
+            del current
+            return
+
+        while current:
+            if current.value == value:
+                break
+            previous = current
+            current = current.next
+        if current is not None:
+            previous.next = current.next
+            del current
+
 
 
 
 if __name__ == '__main__':
     # A test linked list and its nodes
-    node_3 = Node(100)
+    node_4 = Node(999)
+    node_3 = Node(100, node_4)
     node_2 = Node(23, node_3)
     node_1 = Node(432, node_2)
 
     linked_list = LinkedList(node_1)
 
-
-    print(linked_list.getItem(0))
+    print(linked_list.getItem(3))
     print(linked_list.search(432))
     print(linked_list.search(0))
 
-    linked_list.insertFront(92)
-    print(linked_list.getItem(0))
-    print(linked_list.getItem(3))
-    
+    print(linked_list)
+    linked_list.remove(100)
+    print(node_4)
+    print(linked_list)
+    linked_list.remove(432)
+    print(linked_list)
