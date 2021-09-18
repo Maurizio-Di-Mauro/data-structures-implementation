39400ed03cf8d193fbf063e9aa489685ae9081e9
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-09-18 13:05:50 +0300

    add five basic operations

--- stack_adt.py
+++ stack_adt.py
@@ -1,17 +1,50 @@
 # Stack ADT: Last In First Out
 
-class Stack_List_Imp(object):
+class Stack_List_Imp:
     """This is a simple Stack Abstract Data Type
     implemented by using fixed array"""
-    def __init__(self, max_size: int, items: [int] = []):
-        if len(items) > max_size:
-            raise Exception("List is bigger than max size")
+    def __init__(self, maxsize: int, items: [int] = []):
+        if len(items) > maxsize:
+            raise Exception("List is bigger than maxsize")
 
-        self.max_size: int = max_size
-        self.items: [int] = items
+        self.maxsize: int = maxsize
+        self.items: [int] = items.copy()
+        self.length: int = len(items)
 
     # We have two principal operations:
     # 
     # push - add an element to the start
     #
     # pop - remove element from the start
+    #
+    # Also we will have three additional ones: 
+    # top() - return top value without popping it
+    # is_empty() - check whether the stack is empty or not
+    # is_full() - check whether the stack is full or not
+
+    def push(self, value: int):
+        if self.is_full():
+            raise Exception("Overflow error. Exceeded the max size!")
+
+        self.items[length] = value
+        self.length += 1
+
+    def pop(self) -> int:
+        if self.is_empty():
+            raise Exception("Underflow error. Empty stack")
+
+        self.length -= 1
+        return self.items[length]
+
+    def top(self) -> int:
+        if self.is_empty():
+            raise Exception("Empty stack")
+
+        return self.items[length-1]
+
+    def is_empty(self) -> bool:
+        return self.length == 0
+
+    def is_full(self) -> bool:
+        return self.length == self.maxsize
+        
