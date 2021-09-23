6958bc48a81c2492cd422e166eade71da975b5da
Maurizio-Di-Mauro <28500927+Maurizio-Di-Mauro@users.noreply.github.com>
2021-09-23 23:07:17 +0300

    Linked List Stack ADT implementation

--- stack_adt.py
+++ stack_adt.py
@@ -11,6 +11,9 @@ class Stack_List_Imp:
         self.items: [int] = items.copy()
         self.length: int = len(items)
 
+    def __len__(self) -> int:
+        return self.length
+
     # We have two principal operations:
     # 
     # push - add an element to the start
@@ -47,3 +50,48 @@ class Stack_List_Imp:
 
     def is_full(self) -> bool:
         return self.length == self.maxsize
+
+
+
+
+class Node:
+    """This is a simple Node of a Linked List"""
+    def __init__(self, value: int, next: "Node" = None):
+        self.value: int = value
+        self.next = next
+
+
+class Stack_Linked_List_Imp:
+    """This is a simple Linked List Stack ADT Implementation"""
+    def __init__(self, items: [int]):
+        self.length: int = 0
+        self.head: Node = None
+        for item in items:
+            self.push(item)
+
+    def __len__(self) -> int:
+        return self.length
+
+    def push(self, value: int):
+        new_head = Node(value, self.head)
+        self.head = new_head
+        self.length += 1
+
+    def pop(self) -> int:
+        if self.is_empty():
+            raise Exception("Underflow error. Empty stack")
+        return_value: int = self.head.value
+        self.head = self.head.next
+        # here should have been some deletions of unused nodes
+        # ...
+        self.length -= 1
+        return return_value
+
+    def is_empty(self) -> bool:
+        return self.head == None
+    
+    def top(self) -> int:
+        if self.is_empty():
+            raise Exception("Empty stack")
+
+        return self.head.value
